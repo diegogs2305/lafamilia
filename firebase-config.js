d59b546de04f99a5255d3dd051a8b5627e938508
diegogs2305 <diegogs2305@gmail.com>
2025-04-25 19:28:34 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_DOMINIO.firebaseapp.com",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_BUCKET.appspot.com",
+  messagingSenderId: "TU_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
