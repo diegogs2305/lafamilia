8a1149cf66ff9eb2a3ae46193b00b93419bf8871
diegogs2305 <diegogs2305@gmail.com>
2025-05-06 17:15:56 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,1 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCZL5J8KvXq4O-siJFivVkOCqOpojcBoKM",
-  authDomain: "pagina-web-b25b5.firebaseapp.com",
-  projectId: "pagina-web-b25b5",
-  storageBucket: "pagina-web-b25b5.firebasestorage.app",
-  messagingSenderId: "491845378296",
-  appId: "1:491845378296:web:e1827a07d157fa05c8db0d",
-  measurementId: "G-86F0EJ46LS"
-};
+
